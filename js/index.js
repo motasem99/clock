c2c84682c2afa3ac302d843cc9b2fae50b418787
motasem <motasemimad10@gmail.com>
2020-02-26 21:28:11 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+var diallines = document.getElementsByClassName('diallines');
+var clockE1 = document.getElementsByClassName('clock')[0];
+for (var i = 1; i < 60; i++) {
+  clockE1.innerHTML += "<div class='diallines'> </div>";
+  diallines[i].style.transform = 'rotate(' + 6 * i + 'deg)';
+}
+function clock() {
+  var weekday = new Array(7),
+    d = new Date(),
+    h = d.getHours(),
+    m = d.getMinutes(),
+    s = d.getSeconds(),
+    date = d.getDate(),
+    month = d.getMonth() + 1,
+    year = d.getFullYear(),
+    hDeg = h * 30 + m * (360 / 720),
+    mDeg = m * 6 + s * (360 / 3600),
+    sDeg = s * 6,
+    hE1 = document.querySelector('.hour-hand'),
+    mE1 = document.querySelector('.minute-hand'),
+    sE1 = document.querySelector('.second-hand'),
+    dateE1 = document.querySelector('.date'),
+    dayE1 = document.querySelector('.day');
+
+  weekday[0] = 'sunday';
+  weekday[1] = 'monday';
+  weekday[2] = 'tuesday';
+  weekday[3] = 'wednesday';
+  weekday[4] = 'thursday';
+  weekday[5] = 'friday';
+  weekday[6] = 'saturday';
+
+  var day = weekday[d.getDay()];
+  hE1.style.transform = 'rotate(' + hDeg + 'deg)';
+  mE1.style.transform = 'rotate(' + mDeg + 'deg)';
+  sE1.style.transform = 'rotate(' + sDeg + 'deg)';
+  dateE1.innerHTML = date + '/' + month + '/' + year;
+  dayE1.innerHTML = day;
+}
+setInterval('clock()', 100);
